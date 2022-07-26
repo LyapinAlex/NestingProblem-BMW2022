9a82152345e7a9ad2ffb9df73f4141ac4bbbc6f5
LyapinAlex <95094238+LyapinAlex@users.noreply.github.com>
2022-07-26 15:14:13 +0700

    Update Problem.py

---
+++ Problem.py
@@ -0,0 +1,18 @@
+w = 100
+l = 100
+n = 10
+
+
+
+class Problem():
+
+    def __init__(self, name="", len = l, width = w, number = n):
+        self.len = len
+        self.width = width
+        self.number = number
+
+        self.setOfPlane = []
+        self.setOf
+
+
+
