41178944cc7ccebcae59920fcddf5280ca0c9a8a
LyapinAlex <95094238+LyapinAlex@users.noreply.github.com>
2022-07-27 00:17:06 +0700

    Update Problem.py

--- Problem.py
+++ Problem.py
@@ -1,18 +1,46 @@
+# import  rastr_method as rastrMethod 
+import data.generate 
 w = 100
 l = 100
 n = 10
+
+class Pallet():
+    def __init__(len = l, width = w, e = None):
+        self.len = len
+        self.width = width
+
+        self.suffer = list
+
+
 
 
 
 class Problem():
 
-    def __init__(self, name="", len = l, width = w, number = n):
-        self.len = len
-        self.width = width
+    def __init__(self, name ="", len = l, width = w, number = n):
+        self.name = name
         self.number = number
 
-        self.setOfPlane = []
-        self.setOf
+
+        self.useMethod = 0
+        self.solution = {}
+        self.listOfFigure = []
+        return
+
+    def chooceMethog(self):
+        self.
+        self.useMethod = 1
+        return
+
+    def setListOfFiguare(self):
+
+
+    def rastrMethod(self, ):
+        
+
+
+
+
 
 
 
