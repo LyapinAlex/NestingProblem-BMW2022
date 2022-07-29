8167633bb79996a40f5c52f6f873db5b9d41cf40
SofiaShperling <73811581+SofiaShperling@users.noreply.github.com>
2022-07-29 16:20:56 +0700

    add DrawSolution function

---
+++ DrawSolution.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+from matplotlib import patches
+
+def draw_pallet(items, pallet_width, pallet_height):
+    # fig, ax = plt.subplots(figsize=(pallet_height, pallet_height))
+    fig, ax = plt.subplots()
+    pallet = patches.Rectangle((0, 0), pallet_width, pallet_height, linewidth=2, facecolor='none', edgecolor='blue')
+    ax.add_patch(pallet)
+    ax.set_xlim(-1, pallet_width + 1)
+    ax.set_ylim(-1, pallet_height + 1)
+    # ax.grid(True)
+    for item in items:
+        for point in item.points:
+            point[0] += item.lb_x
+            point[1] += item.lb_y
+        polygon = patches.Polygon(item.points)
+        ax.add_patch(polygon)
+    return fig, ax
