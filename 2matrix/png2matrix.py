e6421da7fa68bb30c46b30ad42aa270f79daa968
Vsevolod86 <83454359+Vsevolod86@users.noreply.github.com>
2022-08-02 14:57:02 +0700

    png2matrix

--- 2matrix/png2matrix.py
+++ 2matrix/png2matrix.py
@@ -1,8 +1,8 @@
 import numpy as np
+from PIL import Image
 
-
-def png2matrix(img):
-    numpydata = np.asarray(img)
+def png2matrix(img): # ввод: сырая ссылка на объект png2matrix(r"2matrix/chel.png")
+    numpydata = np.asarray(Image.open(img))
     matrix = np.ones((numpydata.shape[0], numpydata.shape[1]), dtype="int")
     for i in range(0, numpydata.shape[0]):
         for j in range(0, numpydata.shape[1]):
@@ -10,3 +10,5 @@ def png2matrix(img):
                     and numpydata[i][j][2] == 255):
                 matrix[i][j] = 0
     return matrix
+
+# print(png2matrix(r"2matrix/chel.png"))
