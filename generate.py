2653888b64a6072ec72cd9203ea58ca8492a6831
LyapinAlex <95094238+LyapinAlex@users.noreply.github.com>
2022-08-05 12:52:29 +0700

    rename generator

--- generate.py
+++ generate.py
@@ -19,53 +19,6 @@ class Generator():
         return
 
 
-    # def start(self, e):
-
-    #     # pp = random.randint(3, 8)
-    #     pp = 4
-
-    #     # showPolygon(arpol(getPolygon(), 1.0, 2.0, 10))
-   
-    #     for id in range(self.number):
-    #         points = np.array(uc.arpol(uc.getPolygon(), 3, 5, 4))
-    #         item = class_item.Item(id, points)
-    #         x = random.gauss(5, 1.5) 
-    #         # print(item.points) 
-
-    #         for point in item.points:
-      
-    #             point[0]*=x
-    #             point[1]*=x
-                
-    #         item.set_matrix(e)
-    #         # print(item.points) 
-    #         # исправь для всех сторон
-    #         long = max( len(item.matrix), len(item.matrix[0]) )
-            
-    #         S = sum(sum(item.matrix))*e*e
-
-    #         while long*e > self.len or long*e > self.width :
-    #         # while long*e > self.len or long*e > self.width or S < 1.0 :
-    #             # print("!")
-    #             points = uc.arpol(uc.getPolygon(),3 ,5, 4)
-    #             item = class_item.Item(id, points)
-    #             x = random.gauss(5, 1.5)    
-    #             for point in item.points:
-    #                 point[0]*=x
-    #                 point[1]*=x
-    #             item.set_matrix(e)
-    #             long = max( len(item.matrix), len(item.matrix[0]) )
-    #             S = sum(sum(item.matrix))*e*e
-
-    #         # print(item.points)
-            
-    #         item.surfPoint()
-    #         item.points = item.points.tolist()
-    #         self.data.append(item)
-           
-
-    #     return 
-
     def start(self, e):
 
         # pp = random.randint(3, 8)
@@ -74,12 +27,16 @@ class Generator():
         # showPolygon(arpol(getPolygon(), 1.0, 2.0, 10))
    
         for id in range(self.number):
-            x = random.gauss(5, 2.5) 
-            y = random.gauss(5, 2.5)   
-            points = np.array([[0, 0], [x, 0], [x, y], [0, y]])
+            points = np.array(uc.arpol(uc.getPolygon(), 3, 5, 4))
             item = class_item.Item(id, points)
+            x = random.gauss(5, 1.5) 
             # print(item.points) 
 
+            for point in item.points:
+      
+                point[0]*=x
+                point[1]*=x
+                
             item.set_matrix(e)
             # print(item.points) 
             # исправь для всех сторон
@@ -89,15 +46,45 @@ class Generator():
 
             while long*e > self.len or long*e > self.width :
             # while long*e > self.len or long*e > self.width or S < 1.0 :
-                x = random.gauss(5, 2.5)  
-                y = random.gauss(5, 2.5)  
+                # print("!")
+                points = uc.arpol(uc.getPolygon(),3 ,5, 4)
+                item = class_item.Item(id, points)
+                x = random.gauss(5, 1.5)    
+                for point in item.points:
+                    point[0]*=x
+                    point[1]*=x
+                item.set_matrix(e)
+                long = max( len(item.matrix), len(item.matrix[0]) )
+                S = sum(sum(item.matrix))*e*e
+
+            # print(item.points)
+            
+            item.surfPoint()
+            item.points = item.points.tolist()
+            self.data.append(item)
+           
+
+        return 
+
+    def startRectangles(self, e):
+
+
+   
+        for id in range(self.number):
+            x = random.gauss(3, 1.5) 
+            y = random.gauss(3, 1.5)  
+            points = np.array([[0, 0], [x, 0], [x, y], [0, y]])
+            item = class_item.Item(id, points)
+            item.set_matrix(e)
+            long = max( len(item.matrix), len(item.matrix[0]) )
+
+            while long*e > self.len or long*e > self.width :
+                x = random.gauss(3, 1.5) 
+                y = random.gauss(3, 1.5)   
                 points = np.array([[0, 0], [x, 0], [x, y], [0, y]])
                 item = class_item.Item(id, points)
                 item.set_matrix(e)
                 long = max( len(item.matrix), len(item.matrix[0]) )
-                # S = sum(sum(item.matrix))*e*e
-
-            # print(item.points)
             
             item.surfPoint()
             item.points = item.points.tolist()
