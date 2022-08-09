42ccb7bd7edf719c838a389388c5cdcb6d2e736e
Vsevolod86 <83454359+Vsevolod86@users.noreply.github.com>
2022-08-09 10:42:26 +0700

    Change class's methods
    Сделал более читабельным код удалив лишнее

--- class_item.py
+++ class_item.py
@@ -35,177 +35,12 @@ class Item:
                               dtype="int")
         return None
 
-    # def shift2zero(self):
-    #     # сдвиг фигуры к началу координат
-    #     x_max = max(self.points[0][0], self.points[1][0])
-    #     x_min = min(self.points[0][0], self.points[1][0])
-    #     y_max = max(self.points[0][1], self.points[1][1])
-    #     y_min = min(self.points[0][1], self.points[1][1])
-    #     for i in range(0, (self.points).shape[0]):
-    #         x_max = max(x_max, self.points[i][0])
-    #         y_max = max(y_max, self.points[i][1])
-    #         x_min = min(x_min, self.points[i][0])
-    #         y_min = min(y_min, self.points[i][1])
-    #     # нормировка фигуры к (0,0)
-    #     for i in range(0, (self.points).shape[0]):
-    #         self.points[i][0] -= x_min
-    #         self.points[i][1] -= y_min
-    #     return [x_max - x_min, y_max - y_min]
-
     def set_matrix(self, h):
-        # вычисление размера массива
-        size_of_sides = self.shift2zero()
-        n_x = math.ceil(size_of_sides[0] / h)
-        n_y = math.ceil(size_of_sides[1] / h)
-
-        # заполнение массива пересечений с осями параллельными оси абсцисс
-        edges = np.zeros((n_x + 1, n_y))
-        for k in range(0, n_y):
-            for i in range(0, (self.points).shape[0]):
-                j = (i + 1) % (self.points).shape[0]
-                if ((min(self.points[i][1], self.points[j][1]) <= k * h) and
-                    (k * h <= max(self.points[i][1], self.points[j][1]))):
-                    i1 = np.copy(self.points[i])  # i - номер точки i1
-                    i2 = np.copy(self.points[j])  # j - номер точки i2
-                    if (i2[1] - i1[1] == 0):  #ребро параллельно оси абсцисс
-                        if (i1[0] > i2[0]):
-                            fr = i1[0]
-                            i1[0] = i2[0]
-                            i2[0] = fr
-                        edges[math.floor(i1[0] / h)][k] += 1.33
-                        if (i2[0] % h != 0):
-                            edges[math.floor(i2[0] / h + h / 100)][k] += 1.33
-                        else:
-                            edges[math.floor(i2[0] / h + h / 100)][k] += 1.33
-
-                    else:  #x+ay+b=0
-                        a = -(i2[0] - i1[0]) / (i2[1] - i1[1])
-                        b = -i1[0] - i1[1] * a
-                        y_p = k * h
-                        x_p = round(-b - a * k * h, 8)
-                        # проверка положения по разные стороны
-                        if (x_p == i1[0]):
-                            # относительно первой точки
-                            if ((i2[1] - y_p) *
-                                (self.points[(i - 1) % (self.points).shape[0]][1] - y_p) < 0):
-                                edges[math.floor(i1[0] / h + h / 100)][k] += 1
-                            elif ((i2[1] - y_p) * 
-                                (self.points[(i - 1) % (self.points).shape[0]][1] - y_p) > 0):
-                                edges[math.floor(i1[0] / h + h / 100)][k] += 2
-                        elif (x_p != i2[0]):
-                            edges[math.floor(x_p / h + h / 100)][k] += 1
-                            #print(i1, i2, x_p)
-        # print(edges)
-        # создание растровой копии
-        self.matrix = np.zeros((n_x + 1, n_y + 1), dtype="int")
-        for k in range(0, n_y):
-            flag = 0.0
-            for i in range(0, n_x + 1):
-                if ((edges[i][k] % 2 == 0) and
-                    (edges[i][k] != 0)):  #если наталкнулись на угол и т.п.
-                    self.matrix[i][k] = 1
-                elif (edges[i][k] % 2 == 1):  #если наталкнулись на пересечение
-                    self.matrix[i][k] = 1
-                    if (k - 1 >= 0): self.matrix[i][k - 1] = 1
-                    if (flag == 0): flag = 1
-                    else: flag = 0
-                elif (edges[i][k] % 1 !=
-                      0):  #если наталкнулись на плотное косание
-                    if (k - 1 >= 0):
-                        if (self.matrix[i][k - 1] == 0):
-                            self.matrix[i][k] = 1
-                        if ((flag == 0.5) and (i - 1 >= 0)):
-                            if (self.matrix[i - 1][k] == 1):
-                                self.matrix[i][k] = 1
-                    else:
-                        self.matrix[i][k] = 1
-                    if (flag == 0.5): flag = 0
-                    else: flag = 0.5
-                if (flag > 0):  #заливка
-                    self.matrix[i][k] = 1
-                    if ((flag == 1) and (k - 1 >= 0)):
-                        self.matrix[i][k - 1] = 1
-        # закрашивание границ
-        for i in range(0, (self.points).shape[0]):
-            j = (i + 1) % (self.points).shape[0]
-            i1 = np.copy(self.points[i])  # i - номер точки i1
-            i2 = np.copy(self.points[j])  # j - номер точки i2
-
-            j1 = [int(i1[0] / h), int(i1[1] / h)]  # пиксель соотв. i1 точке
-            j2 = [int(i2[0] / h), int(i2[1] / h)]  # пиксель соотв. i2 точке
-
-            step_x = 1
-            step_y = 1
-            check = 1
-            if (j1[0] > j2[0]):
-                step_x = -1
-            elif (j1[0] == j2[0]):
-                step_x = 0
-            if (j1[1] > j2[1]):
-                step_y = -1
-                check = 0
-            elif (j1[1] == j2[1]):
-                step_y = 0
-
-            p = [int(i1[0] / h), int(i1[1] / h)]  # двигается от j1 к j2
-            if (j1[1] == j2[1]):  # вертикальная граница
-                for i in range(0, abs(j1[0] - j2[0]) + 1):
-                    self.matrix[p[0], p[1]] = 1
-                    p[0] += step_x
-            else:
-                for i in range(0, abs(j1[0] - j2[0]) + abs(j1[1] - j2[1]) + 1):
-                    self.matrix[p[0], p[1]] = 1
-                    if (j2 != p):
-                        a = -(i2[0] - i1[0]) / (i2[1] - i1[1])
-                        b = -i1[0] - i1[1] * a
-                        x_p = round(-b - a * (p[1] + check) * h, 8)
-                        if (int(x_p / h) == p[0]):
-                            p[1] += step_y
-                        else:
-                            p[0] += step_x
-        self.matrix = self.matrix[0:n_x, 0:n_y]
+        self.matrix = polygon2matrix(self.points, h)
         return None
 
-    def matrix_of_border(self, h, n_x, n_y):
-        mat = np.zeros((n_x + 1, n_y + 1), dtype="int")
-        for i in range(0, (self.points).shape[0]):
-            j = (i + 1) % (self.points).shape[0]
-            i1 = np.copy(self.points[i])  # i - номер точки i1
-            i2 = np.copy(self.points[j])  # j - номер точки i2
-
-            j1 = [int(i1[0] / h), int(i1[1] / h)]  # пиксель соотв. i1 точке
-            j2 = [int(i2[0] / h), int(i2[1] / h)]  # пиксель соотв. i2 точке
-
-            step_x = 1
-            step_y = 1
-            check = 1
-            if (j1[0] > j2[0]):
-                step_x = -1
-            elif (j1[0] == j2[0]):
-                step_x = 0
-            if (j1[1] > j2[1]):
-                step_y = -1
-                check = 0
-            elif (j1[1] == j2[1]):
-                step_y = 0
-
-            p = [int(i1[0] / h), int(i1[1] / h)]  # двигается от j1 к j2
-            if (j1[1] == j2[1]):  # вертикальная граница
-                for i in range(0, abs(j1[0] - j2[0]) + 1):
-                    mat[p[0], p[1]] = 1
-                    p[0] += step_x
-            else:
-                for i in range(0, abs(j1[0] - j2[0]) + abs(j1[1] - j2[1]) + 1):
-                    mat[p[0], p[1]] = 1
-                    if (j2 != p):
-                        a = -(i2[0] - i1[0]) / (i2[1] - i1[1])
-                        b = -i1[0] - i1[1] * a
-                        x_p = round(-b - a * (p[1] + check) * h, 8)
-                        if (int(x_p / h) == p[0]):
-                            p[1] += step_y
-                        else:
-                            p[0] += step_x
-        mat = mat[0:n_x, 0:n_y]
+    def matrix_of_border(self, h):
+        mat = polyline2matrix(self.points, h)
         return mat
 
     def set_rotation(self, rotate):
@@ -216,31 +51,33 @@ class Item:
             print("Не прямой поворот:", self.rotation)
         return None
 
-    def list_of_ShiftC_4R(self, h): # крутит против часовой стрелки
+    def list_of_ShiftC_4R(self, h):  # крутит против часовой стрелки
         self.set_matrix(h)
-        li=np.array( [[simple2revers_shift(self.matrix),simple2shift(self.matrix)],[None,None],[None,None],[None,None]] )
-        for i in range(1,4):
+        li = np.array(
+            [[simple2revers_shift(self.matrix),
+              simple2shift(self.matrix)], [None, None], [None, None],
+             [None, None]])
+        for i in range(1, 4):
             li[i][0] = simple2revers_shift(np.rot90(self.matrix, i))
             li[i][1] = simple2shift(np.rot90(self.matrix, i))
         return li
 
-    def list_of_MixedShiftC_4R(self, h): # крутит против часовой стрелки
+    def list_of_MixedShiftC_4R(self, h):  # крутит против часовой стрелки
         self.set_matrix(h)
-        li=np.array( [[simple2mixed_shift(self.matrix)],[None],[None],[None]] )
-        for i in range(1,4):
+        li = np.array([[simple2mixed_shift(self.matrix)], [None], [None],
+                       [None]])
+        for i in range(1, 4):
             li[i][0] = simple2mixed_shift(np.rot90(self.matrix, i))
         return li
 
 
-eq1 = Item(1, np.array([[1, 0], [0.3, 3], [3, 3.7], [2.1, 0]]))
-print(polyline2matrix(eq1.points, 0.2))
+# eq1 = Item(1, np.array([[1, 0], [0.3, 3], [3, 3.7], [2.1, 0]]))
 # print(eq1.matrix.shape)
 # eq1.set_matrix(0.2)
 # print(eq1.matrix)
-# eq1.shiftCode()
 
-# eq2 = Item(1, np.array([[0.3, 0], [0, 1], [0.7, 1.5], [1.2, 0.8], [3, 0.8], [3, 0.4], [1.2, 0.4], [0.6, 0.8]]))
-# start_time=time.time()
-# # print(eq2.listOf4Mat2Cod(0.025))
-# print(eq2.list_of_MixedShiftC_4R(0.4))
-# print(time.time() - start_time, " seconds")
+eq2 = Item(1, np.array([[0.3, 0], [0, 1], [0.7, 1.5], [1.2, 0.8], [3, 0.8], [3, 0.4], [1.2, 0.4], [0.6, 0.8]]))
+start_time=time.time()
+# print(eq2.listOf4Mat2Cod(0.025))
+print(eq2.list_of_MixedShiftC_4R(0.4))
+print(time.time() - start_time, " seconds")
