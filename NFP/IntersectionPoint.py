041d4b7cc30bfb4b7c7f26a04b2cd76fc1711ee9
SofiaShperling <73811581+SofiaShperling@users.noreply.github.com>
2022-08-10 23:22:07 +0700

    create function to calculate intersection point

---
+++ NFP/IntersectionPoint.py
@@ -0,0 +1,27 @@
+from __future__ import division
+
+
+def line(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    return A, B, -C
+
+
+def intersection_point(points1, points2):
+    """"
+    Function to count the point of intersection of two lines
+    """
+    L1 = line(points1[0], points1[1])
+    L2 = line(points2[0], points2[1])
+    D = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        x = Dx / D
+        y = Dy / D
+        # print('Hello')
+        return x, y
+    else:
+        return False
+
