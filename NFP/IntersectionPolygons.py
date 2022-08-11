92606fdba8a13dbed1d58d0ce3fd0b76503e0195
SofiaShperling <73811581+SofiaShperling@users.noreply.github.com>
2022-08-11 15:20:28 +0700

    optimize function to find an intersection of polygon by the edge by adding conditions

--- NFP/IntersectionPolygons.py
+++ NFP/IntersectionPolygons.py
@@ -3,17 +3,18 @@ from NFP.IntersectionPoint import intersection_point
 
 def intersection_polygon_by_edge(item, edge):
     points = []
-    # print(item[0].coordinate[0])
     for vertex in item:
         point = False
-        # можно улучшить написав условия, когда может пересекаться
-        # if (np.min([vertex.coordinate[0], vertex.edges[0][0]]) > np.max([edge.coordinate[0], edge.edges[0][0]])) or (np.max([vertex.coordinate[0], vertex.edges[0][0]]) < np.min([edge.coordinate[0], edge.edges[0][0]])) or not(
-        #     np.min([vertex.coordinate[1], vertex.edges[0][1]]) > np.max([edge.coordinate[1], edge.edges[0][1]])) or ( np.max([vertex.coordinate[1], vertex.edges[0][1]]) < np.min([edge.coordinate[1], edge.edges[0][1]])):
-        #     print('здесь')
-        #     point = intersection_point(vertex, edge)
-        #     points.append(point)
-        point = intersection_point(vertex, edge)
-        if point:
+        if not (vertex.coordinate[0] > edge.coordinate[0] and vertex.edges[0][0] > edge.coordinate[0]
+                        and vertex.coordinate[0] > edge.edges[0][0] and vertex.edges[0][0] > edge.edges[0][0]) and not (
+                vertex.coordinate[0] < edge.coordinate[0] and vertex.edges[0][0] < edge.coordinate[0]
+                        and vertex.coordinate[0] < edge.edges[0][0] and vertex.edges[0][0] < edge.edges[0][0]) and not (
+                vertex.coordinate[1] > edge.coordinate[1] and vertex.edges[0][1] > edge.coordinate[1]
+                        and vertex.coordinate[1] > edge.edges[0][1] and vertex.edges[0][1] > edge.edges[0][1]) and not (
+                vertex.coordinate[1] < edge.coordinate[1] and vertex.edges[0][1] < edge.coordinate[1]
+                        and vertex.coordinate[1] < edge.edges[0][1] and vertex.edges[0][1] < edge.edges[0][1]):
+            print('здесь')
+            point = intersection_point(vertex, edge)
             points.append(point)
     if points:
         return points
