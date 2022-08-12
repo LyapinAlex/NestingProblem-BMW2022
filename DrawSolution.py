19c72255d2eeecf4720deac476f224c96ae31600
Vsevolod86 <83454359+Vsevolod86@users.noreply.github.com>
2022-08-12 18:38:06 +0700

    Upd: draw_all_pallets
    пиксели на рисунке рисуем

--- DrawSolution.py
+++ DrawSolution.py
@@ -1,10 +1,11 @@
 from matplotlib import pyplot as plt
 from matplotlib import patches
+import numpy as np
 import math
 
-def draw_pallet(items, pallet_width, pallet_height):
+def draw_pallet(items, pallet_width, pallet_height, h):
     fig, ax = plt.subplots()
-    MAX_SIZE = 7
+    MAX_SIZE = 20
     if pallet_width > pallet_height:
         fig.set_figheight(MAX_SIZE * pallet_height/pallet_width)
         fig.set_figwidth(MAX_SIZE)
@@ -15,8 +16,8 @@ def draw_pallet(items, pallet_width, pallet_height):
    
     pallet = patches.Rectangle((0, 0), pallet_width, pallet_height, linewidth=2, facecolor='none', edgecolor='black')
     ax.add_patch(pallet)
-    ax.set_xlim(-0.5, pallet_width + 0.5)
-    ax.set_ylim(-0.5, pallet_height + 0.5)
+    ax.set_xlim(-0.5, pallet_width + 2)
+    ax.set_ylim(-0.5, pallet_height + 2)
 
     for item in items:
         for point in item.points:
@@ -29,14 +30,22 @@ def draw_pallet(items, pallet_width, pallet_height):
             point[0] += item.lb_x
             point[1] += item.lb_y
    
-        polygon = patches.Polygon(item.points, linewidth=1, facecolor='silver', edgecolor='black')
+        for j in range(item.matrix.shape[1]):
+            for i in range(item.matrix.shape[0]):
+                if item.matrix[i][j]:
+                    sqver = np.array([[i, j], [i+1, j], [i+1, j+1], [i, j+1]])*h
+                    for i in range(sqver.shape[0]):
+                        sqver[i][0] += item.lb_x
+                        sqver[i][1] += item.lb_y
+                    polygon = patches.Polygon(sqver, linewidth=1, facecolor='silver', edgecolor='black', alpha = 0.33)
+                    ax.add_patch(polygon)
+        polygon = patches.Polygon(item.points, linewidth=1, edgecolor='red', fill = False)
         ax.add_patch(polygon)
         
     plt.savefig('pallet' + str(items[0].pallet_number) + '.png')
     return fig, ax
 
-def draw_all_pallets(packing, pallet_width, pallet_leight):
+def draw_all_pallets(packing, pallet_width, pallet_leight, h):
     
     for i in range(len(packing)):
-      
-        draw_pallet(packing[i], pallet_width, pallet_leight)
+        draw_pallet(packing[i], pallet_width, pallet_leight, h)
--- pallet.py
+++ pallet.py
@@ -1,12 +1,11 @@
 import class_item
 import numpy as np
-import math
 
 class Pallet():
 
     def __init__(self, id, w, l, e):
         
-        self.shape = (math.ceil(w / e), math.ceil(l / e))  
+        self.shape = (int(w / e), int(l / e))  
         self.id = id
         self.matrix = np.zeros(self.shape, dtype =  np.uint16)
         self.items = []
