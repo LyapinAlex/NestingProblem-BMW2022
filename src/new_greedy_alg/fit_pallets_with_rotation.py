b4e1514fde53f8ca6f87e00f69d0b123a0382f83
Vsevolod86 <83454359+Vsevolod86@users.noreply.github.com>
2022-09-25 19:33:22 +0700

    Update fit_pallets_with_rotation.py

--- src/new_greedy_alg/fit_pallets_with_rotation.py
+++ src/new_greedy_alg/fit_pallets_with_rotation.py
@@ -19,24 +19,24 @@ def fit_pallets_with_rotation(pallet_shape, items, h):
         x = 0
         y = 0
         rotation = 0
-        number_pallet_line = -1
+        pallet_number = -1
         is_placed_item = False
-        while not is_placed_item and number_pallet_line < len(pallets) - 1:
-            number_pallet_line += 1
-            is_placed_item, x, y, rotation = find_position_with_rotation(pallets[number_pallet_line], pallet_shape, item)
+        while not is_placed_item and pallet_number < len(pallets) - 1:
+            pallet_number += 1
+            is_placed_item, x, y, rotation = find_position_with_rotation(pallets[pallet_number], pallet_shape, item)
 
         if not is_placed_item:
             pallets.append(create_pallet(pallet_shape))
-            number_pallet_line += 1
-            is_placed_item, x, y, rotation = find_position_with_rotation(pallets[number_pallet_line], pallet_shape, item)
+            pallet_number += 1
+            is_placed_item, x, y, rotation = find_position_with_rotation(pallets[pallet_number], pallet_shape, item)
 
         if not is_placed_item:
             print("Предмет не влазит в паллету")
         else:
-            item.pallet_number = number_pallet_line
+            item.pallet_number = pallet_number
             item.raster_coord = (x, y)
             item.lb_x = y * h
             item.lb_y = x * h
             item.rotation = rotation * math.pi / 2
-            fit_item(pallets[number_pallet_line], item.list_new_shift[rotation], x, y)
+            fit_item(pallets[pallet_number], item.list_new_shift[rotation], x, y)
     return pallets
